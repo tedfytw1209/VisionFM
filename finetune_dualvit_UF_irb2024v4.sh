097f4b40e7209002245b676cf9d23af376816ac4
teddy <teddytw1209@gmail.com>
2025-08-23 20:30:26 -0400

    update

---
+++ finetune_dualvit_UF_irb2024v4.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4gb
+#SBATCH --partition=hpg-turin
+#SBATCH --gpus=1
+#SBATCH --time=48:00:00
+#SBATCH --output=%x.%j.out
+#SBATCH --account=ruogu.fang
+#SBATCH --qos=ruogu.fang
+
+date;hostname;pwd
+
+module load conda
+conda activate /blue/guoj1/tienyuchang/.conda/envs/vfm
+# Go to home directory
+#cd $HOME
+STUDY=$1 #AMD_all_split 2, Cataract_all_split 2, DR_all_split 6, Glaucoma_all_split 6, DR_binary_all_split 2, Glaucoma_binary_all_split 2
+MODEL="vit_base"
+FINETUNED_MODEL="VisionFM"
+LR=${2:-"1e-3"}
+Num_CLASS=${3:-"2"}
+weight_decay="0.05"
+Eval_score="auc"
+Modality="Dual"
+
+NUM_K=0
+data_type="IRB2024_v4"
+IMG_Path="/orange/ruogu.fang/tienyuchang/IRB2024_imgs_paired/"
+Epochs=100
+OPTIMIZER="adamw" # "adamw" or "sgd"
+BATCH_SIZE=128
+
+MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+
+echo $SUBSTUDY
+echo $Num_CLASS
+
+# Modify the path to your singularity container 
+#sbatch finetune_dualvit_UF_irb2024v4.sh AMD_all_split 1e-3 2 OCT
+python inference_dualvisionfm_for_multiclass_classification_UF.py --oct_pretrained_weights ./results/$STUDY-${data_type}-all-$FINETUNED_MODEL-OCT-bs${BATCH_SIZE}ep${Epochs}lr${LR}opt${OPTIMIZER}-${Eval_score}eval/checkpoint_best_finetune.pth --fundus_pretrained_weights ./results/$STUDY-${data_type}-all-$FINETUNED_MODEL-Fundus-bs${BATCH_SIZE}ep${Epochs}lr${LR}opt${OPTIMIZER}-${Eval_score}eval/checkpoint_best_finetune.pth --arch $MODEL --avgpool_patchtokens 0 --input_size 224 --output_dir ./results/$STUDY-${data_type}-all-$FINETUNED_MODEL-${Modality}-bs${BATCH_SIZE}ep${Epochs}lr${LR}opt${OPTIMIZER}-${Eval_score}eval_test --data_path /orange/ruogu.fang/tienyuchang/OCTRFF_Data/data/UF-cohort/${data_type}/split/tune5-eval5/${STUDY}.csv --task $STUDY-${data_type}-all-$FINETUNED_MODEL-${Modality}-bs${BATCH_SIZE}ep${Epochs}lr${LR}opt${OPTIMIZER}-${Eval_score}eval_test --modality $Modality --num_workers 8 --batch_size_per_gpu $BATCH_SIZE --num_labels $Num_CLASS --extra 10 --img_dir $IMG_Path
--- models/vision_transformer.py
+++ models/vision_transformer.py
@@ -376,3 +376,45 @@ def vit_large(patch_size=16, **kwargs):
         patch_size=patch_size, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4,
         qkv_bias=True, **kwargs)
     return model
+
+class DualViTClassifier(nn.Module):
+    def __init__(self, vit_model_1, classifier_1, vit_model_2, classifier_2, num_classes):
+        """
+        初始化 DualViTClassifier 模型。
+
+        Args:
+            vit_model_1: 第一个 ViT
+            vit_model_2: 第二个 ViT
+            num_classes (int): 分类的类别数。
+        """
+        super(DualViTClassifier, self).__init__()
+        self.num_classes = num_classes
+        self.vit_model_1 = vit_model_1
+        self.classifier_1 = classifier_1
+        self.vit_model_2 = vit_model_2
+        self.classifier_2 = classifier_2
+
+    def forward(self, input_1, input_2):
+        """
+        前向传播。
+
+        Args:
+            input_1 (torch.Tensor): 输入到第一个 ViT 的数据 (batch_size, 3, H, W)。
+            input_2 (torch.Tensor): 输入到第二个 ViT 的数据 (batch_size, 3, D, H, W)。
+
+        Returns:
+            torch.Tensor: 分类结果 (batch_size, num_classes)。
+        """
+        # 第一个 ViT 提取特征 (batch_size, num_classes)
+        features_1 = self.vit_model_1(input_1)
+        # 第二个 ViT 提取特征 (batch_size, num_classes)
+        features_2 = self.vit_model_2(input_2)
+
+        # output mean (batch_size, num_classes)
+        combined_features = (features_1 + features_2) / 2
+
+        return combined_features
+
+def DualViT(**kwargs):
+    model = DualViTClassifier(**kwargs)
+    return model
