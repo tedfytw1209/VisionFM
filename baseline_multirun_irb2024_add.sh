c53c9e6a726efd7c82f471c338fb494cc6926a0e
teddy <teddytw1209@gmail.com>
2025-10-11 22:42:58 -0400

    Update baseline_multirun_irb2024_add.sh

--- baseline_multirun_irb2024_add.sh
+++ baseline_multirun_irb2024_add.sh
@@ -1,4 +1,19 @@
-#! /bin/bash
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4gb
+#SBATCH --partition=hpg-turin
+#SBATCH --gpus=1
+#SBATCH --time=48:00:00
+#SBATCH --output=%x.%j.out
+#SBATCH --account=ruogu.fang
+#SBATCH --qos=ruogu.fang
+
+date;hostname;pwd
+
+module load conda
+conda activate vfm
 
 SCRIPT=$1 #AMD_all_split 2, Cataract_all_split 2, DR_all_split 6, Glaucoma_all_split 6, DR_binary_all_split 2, Glaucoma_binary_all_split 2
 MODEL="vit_base"
@@ -7,8 +22,8 @@ LR=${2:-"1e-3"}
 Num_CLASS=${3:-"2"}
 weight_decay="0.05"
 Eval_score="auc"
-Modality=${4:-"OCT"} # CFP, OCT, OCT_CFP
-SUBSETNUM=${5:-0} # 0, 500, 1000
+Modality=${4:-"OCT"} # Fundus, OCT
+SUBSETNUM=${5:-0} # 0, 500, 1000    
 
 NUM_K=0
 
@@ -21,7 +36,7 @@ do
     # Create a job name based on the variables
     DATASET="${DATASETS[$i]}"
     NUM_CLASS="${CLASSES[$i]}"
-    echo "sbatch $SCRIPT $DATASET $LR $NUM_CLASS $Modality $SUBSETNUM"
-    sbatch $SCRIPT $DATASET $LR $NUM_CLASS $Modality $SUBSETNUM
+    echo "bash $SCRIPT $DATASET $LR $NUM_CLASS $Modality $SUBSETNUM"
+    bash $SCRIPT $DATASET $LR $NUM_CLASS $Modality $SUBSETNUM
     sleep 1 # Optional: sleep to avoid overwhelming the scheduler
 done
