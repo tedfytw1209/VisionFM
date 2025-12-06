1e4dc949ef77a99de13349d49345ad7d75dc20e6
teddy <teddytw1209@gmail.com>
2025-12-05 22:50:40 -0500

    Update baseline_multirun_irb2024.sh

--- baseline_multirun_irb2024.sh
+++ baseline_multirun_irb2024.sh
@@ -33,8 +33,8 @@ echo $SLURM_JOBID
 #sbatch baseline_multirun_irb2024.sh finetune_retfound_UFbenchmark_irb2024v5.sh 1e-3 2 Fundus 0
 #DATASETS=(AMD_all_split Cataract_all_split DR_all_split Glaucoma_all_split DR_binary_all_split Glaucoma_binary_all_split DR_filtered_all_split DR_fbinary_all_split Glaucoma_filtered_all_split Glaucoma_fbinary_all_split)  # List of datasets
 #CLASSES=(2 2 6 6 2 2 5 2 4 2)  # Number of classes for each dataset
-DATASETS=(DR_filtered_all_split DR_fbinary_all_split Glaucoma_filtered_all_split Glaucoma_fbinary_all_split)  # List of datasets
-CLASSES=(5 2 4 2)  # Number of classes for each dataset
+DATASETS=(DR_fbinary_all_split Glaucoma_fbinary_all_split)  # List of datasets
+CLASSES=(2 2)  # Number of classes for each dataset
 for i in "${!DATASETS[@]}"
 do
     # Create a job name based on the variables
